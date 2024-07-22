1d43f76800376bf4c770e77ecb0b1c75ef11c23f
Ajay Sagar <Ajau9560@gmail.com>
2024-07-22 11:30:02 +0530

    first commit of release/1.0.0 branch

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,8 +1,10 @@
 import Body from "./components/body"
+import Header from "./components/header"
 function App() {
   return (
     <>
       <div>
+        <Header />
         <Body />
         <h1>Hello Duniya</h1>
       </div>
---
+++ myApp/src/components/header.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function header() {
+  return <div>Helloo from header</div>
+}
+
+export default header
